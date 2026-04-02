140bcb10315b2e1670a1eba03562652b368babb2
Jatin <patrajatin0@gmail.com>
2026-04-03 02:41:49 +0530

    i add the ui link into the logo of the webpage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,24 @@ function Navbar() {
     return (
         <div className='flex justify-around items-center p-4 bg-pink-500 '>
             <div className='rounded-full border-none   p-4  '>
-                <img src={FRONT_LOGO} alt="" className='w-18 h-18   ' />
+                <Link to={"/"}>
+                    <img src={FRONT_LOGO} alt="" className='w-18 h-18   ' />
+                </Link>
+
 
             </div>
             <div >
                 <ul className='flex gap-2'>
-                    <li className='mx-2 text-white'>Home</li>
-                    <li className='mx-2 text-white'>About</li>
-                    <li className='mx-2 text-white'>cart</li>
+                    <li className='mx-2 text-white font-bold'>Home</li>
+                    <li className='mx-2 text-white font-bold'>About</li>
+                    <li className='mx-2 text-white font-bold'>cart</li>
 
                 </ul>
 
             </div>
             <div>
                 <Link to={"/login"}>
-                    <button className='mx-2  text-white' onClick={() => (setLog(log === "Login" ? "Logout" : "Login"))}>{log}</button>
+                    <button className='mx-2  text-white font-bold cursor-pointer' onClick={() => (setLog(log === "Login" ? "Logout" : "Login"))}>{log}</button>
                 </Link>
 
             </div>
